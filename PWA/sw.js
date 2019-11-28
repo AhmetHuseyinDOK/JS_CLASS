d1d46011d6b27bf1b599c73d11632daf2c57cb01
ahmet <off.ahmethuseyindok@gmail.com>
2019-11-28 13:38:20 +0300

    pwa

---
+++ PWA/sw.js
@@ -0,0 +1,10 @@
+const CACHE ='JS'
+const FILES = ['/JS_CLASS/', '/JS_CLASS/index.html']
+function installCB(e) {
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache => cache.addAll(FILES))
+    .catch(console.log)
+  )
+}
+self.addEventListener('install', installCB)
