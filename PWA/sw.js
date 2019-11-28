1b8742f17a02f808fd47944e8ac1f6ea56e5b7f1
ahmet <off.ahmethuseyindok@gmail.com>
2019-11-28 14:08:13 +0300

    cahce opt

--- PWA/sw.js
+++ PWA/sw.js
@@ -1,5 +1,5 @@
 const CACHE ='JS'
-const FILES = ['/JS_CLASS/', '/JS_CLASS/index.html']
+const FILES = ['/JS_CLASS/PWA/index.html','/JS_CLASS/PWA/']
 function installCB(e) {
   e.waitUntil(
     caches.open(CACHE)
@@ -7,4 +7,15 @@ function installCB(e) {
     .catch(console.log)
   )
 }
+
+function cacheCB(e) { //cache first
+    let req = e.request
+    e.respondWith(
+      caches.match(req)
+      .then(r1 => r1 || fetch(req))
+      .catch(console.log)
+    )
+}
+
+self.addEventListener('fetch', cacheCB)
 self.addEventListener('install', installCB)
